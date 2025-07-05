63e807f0b7dc1945e6d49b9b6b9308b3a7fb61b9
Robin <ThatRobin3001@gmail.com>
2025-07-05 11:06:00 +0100

    InitialCommit

---
+++ src/main/java/io/github/apace100/origins/command/argument/OriginArgumentType.java
@@ -0,0 +1,61 @@
+package io.github.apace100.origins.command.argument;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import io.github.apace100.origins.origin.*;
+import net.minecraft.command.CommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+public class OriginArgumentType implements ArgumentType<Identifier> {
+
+   public static final DynamicCommandExceptionType ORIGIN_NOT_FOUND = new DynamicCommandExceptionType(
+       o -> Text.translatable("commands.origin.origin_not_found", o)
+   );
+
+   public static OriginArgumentType origin() {
+      return new OriginArgumentType();
+   }
+
+   public static <S> Origin getOrigin(CommandContext<S> context, String argumentName) throws CommandSyntaxException {
+      Identifier id = context.getArgument(argumentName, Identifier.class);
+      return OriginManager
+          .getOptional(id)
+          .orElseThrow(() -> ORIGIN_NOT_FOUND.create(id));
+   }
+
+   @Override
+   public Identifier parse(StringReader reader) throws CommandSyntaxException {
+      return Identifier.fromCommandInputNonEmpty(reader);
+   }
+
+   @Override
+   public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+
+      try {
+
+         OriginLayer layer = OriginLayerArgumentType.getLayer(context, "layer");
+         Stream.Builder<Identifier> origins = Stream.builder();
+
+         origins.add(Origin.EMPTY.getId());
+         layer.getOrigins().forEach(origins);
+
+         return CommandSource.suggestIdentifiers(origins.build(), builder);
+
+      }
+
+      catch (Exception e) {
+         return CommandSource.suggestIdentifiers(Stream.of(), builder);
+      }
+
+   }
+
+}
